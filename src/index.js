711acfab3c2877f65e638379b1005b532bddecb5
Kweebac <06.amarshall@gmail.com>
2023-08-17 21:31:30 +0100

    Added my own projects to the templates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "./normalize.css";
 import "./style.css";
 import "./images/battleship.png";
+import "./images/weatherApp.png";
+import "./images/todoList.png";
+import "./images/adminDashboard.png";
